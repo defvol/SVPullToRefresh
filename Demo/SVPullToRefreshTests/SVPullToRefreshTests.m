4670a7bc1c437992265e44f745700ddfc139c412
Rod Wilhelmy <rwilhelmy@gmail.com>
2012-09-12 12:04:13 -0500

    Adds a OCUnit target to the project
    Reference: http://goo.gl/NdgmN

---
+++ Demo/SVPullToRefreshTests/SVPullToRefreshTests.m
@@ -0,0 +1,33 @@
+//
+//  SVPullToRefreshTests.m
+//  SVPullToRefreshTests
+//
+//  Created by Rodolfo Wilhelmy on 9/12/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SVPullToRefreshTests : SenTestCase
+
+@end
+
+#import "SVPullToRefresh.h"
+
+@implementation SVPullToRefreshTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
